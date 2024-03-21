104b099950072206e9d4b57bf50d26db14f9784f
manuelyanez.ar <manuelyanez.ar@gmail.com>
2024-03-21 16:50:36 -0300

    Main configurations to show screens implemented

--- Manager.py
+++ Manager.py
@@ -1,9 +1,31 @@
 # Unique class module
 
 import tkinter as tk
-
+from constants import style
+from screens import Home, Test
 
 class Manager(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("App Contabilidad")
+        container = tk.Frame(self)
+        container.pack(
+            side = tk.TOP,
+            fill = tk.BOTH,
+            expand = True
+        )
+        container.configure(background = style.BACKGROUND)
+        container.grid_columnconfigure(0, weight=1)
+        container.grid_rowconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (Home, Test):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky=tk.NSEW)
+        self.show_frame(Home)
+        
+    def show_frame(self, container):
+        frame = self.frames[container]
+        frame.tkraise()
--- screens.py
+++ screens.py
@@ -1,1 +1,16 @@
 # Multiple classes module for screens
+
+import tkinter as tk
+from constants import style
+
+class Home(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.configure(background = style.BACKGROUND)
+        self.controller = controller
+
+class Test(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.configure(background = style.BACKGROUND)
+        self.controller = controller
